9b98542359966281797f4d54a533772b3f9d6de4
Your Name <you@example.com>
2024-07-02 21:54:46 +0300

    edited task-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,41 +1,49 @@
 const elementsHTML = {
   form: document.querySelector('.feedback-form'),
-  email: document.querySelector('input[type="email"]'),
-  message: document.querySelector('textarea[name="message"]'),
 };
+
 const formData = {
   email: '',
   message: '',
 };
 
-const { form, email, message } = elementsHTML;
-form.addEventListener('input', createUserInput);
+const { form } = elementsHTML;
 
-(() => {
-  const getObjInLocalStorage = JSON.parse(
-    localStorage.getItem('feedback-form-state')
-  );
-  if (getObjInLocalStorage) {
-    email.value = getObjInLocalStorage.email || '';
-    message.textContent = getObjInLocalStorage.message || '';
+form.addEventListener('submit', onSubmitForm);
+form.addEventListener('input', onSaveInputUser);
+
+function onSubmitForm(evt) {
+  evt.preventDefault();
+  if (isEmpty()) {
+    return alert('Fill please all fields');
   }
-})();
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
+  console.log(
+    'KEY in localeStorage "feedback-form-state": ',
+    JSON.parse(localStorage.getItem('feedback-form-state'))
+  );
   localStorage.removeItem('feedback-form-state');
   form.reset();
-  message.textContent = '';
-});
+}
 
-function createUserInput(evt) {
-  const userLabel = evt.target;
-  if (userLabel.nodeName === 'INPUT') {
-    formData.email = evt.target.value;
-  } else if (userLabel.nodeName === 'TEXTAREA') {
-    formData.message = evt.target.value;
-  }
+function onSaveInputUser() {
+  formData.email = form.elements.email.value;
+  formData.message = form.elements.message.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
-function checkStorage() {}
+function checkingStorage() {
+  const userStorage = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (!userStorage) {
+    return;
+  }
+  form.elements.email.value = userStorage.email ?? '';
+  form.elements.message.value = userStorage.message ?? '';
+}
+
+function isEmpty() {
+  return !(
+    form.elements.email.value.trim() && form.elements.message.value.trim()
+  );
+}
+
+checkingStorage();
